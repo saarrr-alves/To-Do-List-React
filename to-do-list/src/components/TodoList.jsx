bc0851100c905d6b3ad6a892de9d3ae2ad73b4a3
saarrr-alves <saraealvesemaia@gmail.com>
2025-11-03 15:08:24 -0300

    Trocando o use de useMemo por memo

--- to-do-list/src/components/TodoList.jsx
+++ to-do-list/src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { memo } from 'react';
 import { useTodos } from '../context/TodoContext';
 import TodoItem from './TodoItem';
 import '../styles/TodoList.css';
@@ -6,11 +6,9 @@ import '../styles/TodoList.css';
 function TodoList () {
     const { filteredTodos } = useTodos();
 
-    const todoList = useMemo(() => (
-        filteredTodos.map(todo => <TodoItem key={todo.id} todo={todo} />)
-    ), [filteredTodos]);
+    const todoList = filteredTodos.map(todo => <TodoItem key={todo.id} todo={todo} />);
 
     return <ul className="list-disc mt-8">{todoList}</ul>;
 };
 
-export default TodoList;
+export default memo(TodoList);
